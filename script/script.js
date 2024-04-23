bfd56981a4f0c6a52832adb656483566b47a1e95
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-23 16:06:08 -0700

    feat(script.js): :construction: add Student Class

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const testBtn = document.querySelector(".test");
 
-// test class 
+//!  test class 
 class Test {
   constructor(message) {
     this.message = message;
@@ -11,9 +11,24 @@ class Test {
 }
 
 
+
+
 testBtn.addEventListener('click', () => {
   let studentNameValue = document.querySelector(".name").value;
   let studentName = new Test(studentNameValue);
   studentName.sayHello();
 }); // test message
 
+//! Student Class 
+/* The class `Student` defines a blueprint for creating student objects with properties such as name,
+lastName, age, subjects, and grades. */
+class Student {
+  
+  constructor(name, lastName, age, subjects, grades) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.subjects = subjects;
+    this.grades = grades;
+  };
+};
