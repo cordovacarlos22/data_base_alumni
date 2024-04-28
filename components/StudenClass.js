8ccc7b73780e43c98b97c5d64c942249ffc31688
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-28 15:40:15 -0700

    feat(COMPONENTS): :construction: working on class methods

--- components/StudenClass.js
+++ components/StudenClass.js
@@ -1,4 +1,11 @@
-class Student {
+
+/* The `Student` class is being imported from the file located at `@/components/StudentClass`. An
+instance of the `Student` class is then created with the following properties: id=1,
+firstName='carlos', lastName='cordova', age=25, subject=',math', and grade=10. The
+`printStudent` method is then called on this instance to presumably display the student's
+information. */
+
+export default class Student {
   constructor(id,name,lastName,age,subjects,grade) {
     this.id = id;
     this.name = name;
@@ -7,4 +14,9 @@ class Student {
     this.subjects = subjects;
     this.grade = grade;
   }
+
+  printStudent() {
+    alert(`Student ${this.name} has been register`)
+  }
 }
+
