5d3309e262ea142aae4f0ba09d4e54b5904a3777
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-28 09:18:38 -0700

    feat(COMPONENTS): :sparkles: ADD STUDEN CLASS

---
+++ components/StudenClass.js
@@ -0,0 +1,10 @@
+class Student {
+  constructor(id,name,lastName,age,subjects,grade) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.subjects = subjects;
+    this.grade = grade;
+  }
+}
