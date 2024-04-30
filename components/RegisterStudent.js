6538f016dc6e29e175052e216b39476815925d1f
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-30 09:47:00 -0700

    feat(created RegisterClass): :sparkles: new method to  alert user register and printStudents added

--- components/RegisterStudent.js
+++ components/RegisterStudent.js
@@ -7,23 +7,26 @@ export default class Register extends Student{
     super(id, name, lastName, age)
   }
   
-  
-  printRegister() {
-    let students = []
+  //add a student to array of students
+  registerStudent() {
+    let students = [];
     students = JSON.parse(localStorage.getItem('studentDB')) || [];
     students.push({ name: this.name, lastName: this.lastName, age: this.age })
     localStorage.setItem('studentDB', JSON.stringify(students));
 
 
-    console.log('student registrado ' + this.name + " " + this.lastName + " " + this.age);
-    console.log('studentDB', localStorage.getItem('studentDB'));
+    // console.log('student registrado ' + this.name + " " + this.lastName + " " + this.age);
+    // console.log('studentDB', localStorage.getItem('studentDB'));
+  }
+
+  printStudent() {
+    alert(`Student ${this.name} has been register`)
   }
 
   getAllStudents() {
-    
-    
-    
-    console.log(students)
+    let students = [];
+    students = JSON.parse(localStorage.getItem('studentDB')) || [];
+    console.log(students);
   }
 
 }
