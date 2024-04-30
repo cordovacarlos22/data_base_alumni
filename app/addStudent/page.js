7f60449570207b8182264b4981a925218c45a6fd
carloscordova.dev <cordovamanuel22@icloud.com>
2024-04-30 08:01:34 -0700

    style(routes): :lipstick: add style to form

--- app/addStudent/page.js
+++ app/addStudent/page.js
@@ -25,32 +25,43 @@ const AddStudent = () => {
 
 
   return (
-    <div className='flex flex-col '>
-      <div>addStudent</div>
-      <form className='border-2  border-red-800 h-48'>
-        <label htmlFor="name">
-          Name:
+    <div className='flex flex-col justify-center items-center m-auto  bg-blue-500  rounded-lg text-white p-6'>
+      <h1 className=' font-bold'>Fill form to register new <span className=' font-extrabold'>Student</span></h1>
+      <form className=' flex flex-col p-4  justify-center text-center items-center  gap-4  m-4'>
+        <label className='text-lg ' htmlFor="name">
+          Name : <br></br>
           <input
+            className='ml-2  text-blue-500  p-2 rounded-lg'
+            required
+            type='text'
             value={name}
             onChange={e => setName(e.target.value)}
             name='name' id='name' placeholder='Enter Your Name'></input>
         </label>
-        <label htmlFor="lastName">
-          Last Name:
+        <label  className="text-lg" htmlFor="lastName">
+          Last Name :<br></br>
           <input
+            required
+            type='text'
+            className='ml-2  text-blue-500  p-2 rounded-lg'
             value={lastName}
             onChange={e => setLastName(e.target.value)}
             name='lastName' id='lastName' placeholder='Enter Your Last Name'></input>
         </label>
-        <label htmlFor="age">
-          age:
+        <label className='text-lg' htmlFor="age">
+          Age :<br></br>
           <input
+            type='number'
+            className='ml-2  text-blue-500  p-2 rounded-lg '
+            required
+
             value={age}
             onChange={e => setAge(e.target.value)}
             name='age' id='age' placeholder='Enter Your Age'></input>
         </label>
+        <button className='bg-white text-blue-500  p-4 rounded-lg hover:text-blue-400 ' onClick={() => { handleRegister() }} ><span className='hover:'>Register Student</span></button>
       </form>
-      <button className='bg-blue-500 w-24 h-24' onClick={() => { handleRegister() }} >Click me to show alert</button>
+      
     </div>
   )
 }
