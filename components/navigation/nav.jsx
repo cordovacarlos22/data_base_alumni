a4e3cf5ae1c35846af5afb06b97194914b143838
carloscordova.dev <cordovamanuel22@icloud.com>
2024-05-01 07:18:17 -0700

    feat(COMPONENTS): :sparkles: creaded link list and map over it to render links and links names

--- components/navigation/nav.jsx
+++ components/navigation/nav.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import Link from 'next/link'
 const Nav = () => {
+  let navLinkList = [{
+    name: 'Home',
+    link: '/'
+  },
+  {
+    name: 'Register Student',
+    link: '/register'
+  },
+  {
+    name: 'Add Subject',
+    link: '/addSubject'
+  }
+
+  ]
   return (
     <header className='bg-blue-500 w-80  h-screen flex flex-col text-white' >
       <h1 className='text-xl  font-bold text-white mx-auto my-4'>Student Dashboard </h1>
       <nav className='  font-semibold p-4 '>
         <ul>
-          <li>
-            <Link href='/' >Home</Link>
-          </li>
-          <li>
-            <Link href='/addStudent' >Register Student</Link>
-          </li>
+          {navLinkList.map((link) => {
+              return (
+                <li key={link.name}>
+                  <Link href={link.link}>
+                    {link.name}
+                  </Link>
+                </li>
+              )
+            })}
         </ul>
       </nav>
     </header>
