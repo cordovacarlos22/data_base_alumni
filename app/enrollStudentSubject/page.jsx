ba7445966ff8a30f7bf577b9ff6583819937bbf5
carloscordova.dev <cordovamanuel22@icloud.com>
2024-05-04 13:24:46 -0700

    fixed bug on enrollStudent route

--- app/enrollStudentSubject/page.jsx
+++ app/enrollStudentSubject/page.jsx
@@ -1,22 +1,26 @@
 'use client'
 import React from 'react'
-
+import Student from '@/components/StudenClass';
 
 const AddSubject = () => {
-  const search = () => {
-  // Student.searchStudent()
-  console.log(localStorage.getItem("students"));
+
+  const search = (e) => {
+    
+    let studentSearch =  new Student();
+    studentSearch.searchStudent()
+
+
   }
   return (
     <div>AddSubject
       <h1>working on it ....</h1>
-      <form>
-     <label htmlFor="studentSearch">
-      Student Search:
-      <input placeholder="Search by ID" name="studentSearch" type="text"/>
-     </label>
-     <button className='bg-white text-blue-500  p-4 rounded-lg hover:text-blue-400 ' onClick={() => { search() }} ><span className=''>Search</span></button>
-      </form>
+
+        <label htmlFor="studentSearch">
+          Student Search:
+          <input placeholder="Search by ID" id='studentSearch' name="studentSearch" type="text" />
+        </label>
+        <button className='bg-white text-blue-500  p-4 rounded-lg hover:text-blue-400 ' onClick={() => { search() }} ><span className=''>Search</span></button>
+      
     </div>
   )
 }
