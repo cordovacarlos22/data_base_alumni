82e12eed937d4e447e36441b0a67b92ca6ad03a3
carloscordova.dev <cordovamanuel22@icloud.com>
2024-05-05 11:42:22 -0700

    feat(routes): :construction: need to improve css

--- app/enrollStudentSubject/page.jsx
+++ app/enrollStudentSubject/page.jsx
@@ -4,17 +4,17 @@ import Student from '@/components/StudenClass';
 
 const AddSubject = () => {
 
- /* The line `let [id, setId] = useState('')` in the code snippet is using the `useState` hook from
- React to declare a state variable named `id` and a function to update that state variable named
- `setId`. */
+  /* The line `let [id, setId] = useState('')` in the code snippet is using the `useState` hook from
+  React to declare a state variable named `id` and a function to update that state variable named
+  `setId`. */
   let [id, setId] = useState('')
 
- /* The line `const [user, setUser] = useState([]);` in the code snippet is using the `useState` hook
- from React to declare a state variable named `user` and a function to update that state variable
- named `setUser`. The initial value of the `user` state variable is set to an empty array `[]`. This
- state variable is used to store and update the user data retrieved from the local storage. When the
- component mounts, it checks if there is any user data stored in the local storage and updates the
- `user` state with that data. */
+  /* The line `const [user, setUser] = useState([]);` in the code snippet is using the `useState` hook
+  from React to declare a state variable named `user` and a function to update that state variable
+  named `setUser`. The initial value of the `user` state variable is set to an empty array `[]`. This
+  state variable is used to store and update the user data retrieved from the local storage. When the
+  component mounts, it checks if there is any user data stored in the local storage and updates the
+  `user` state with that data. */
   const [user, setUser] = useState([]);
 
   /* The `useEffect` hook in the provided code snippet is used to perform side effects in a functional
@@ -28,10 +28,10 @@ const AddSubject = () => {
     }
   }, []);
 
- /**
-  * The `search` function checks if a student ID is provided, searches for the student using the ID,
-  * and updates the user state with the found student.
-  */
+  /**
+   * The `search` function checks if a student ID is provided, searches for the student using the ID,
+   * and updates the user state with the found student.
+   */
   const search = () => {
 
     if (id == '') {
@@ -60,12 +60,20 @@ const AddSubject = () => {
    */
   const handleAddSubject = () => {
     let subject = new Student();
-    let subjectFoundStudent = subject.addSubject("test", user) //  => return student
-
+    let subjectFoundStudent = subject.addSubject(subjectValue, user) //  => return student
+    localStorage.removeItem('userFound');
+    alert("Subject added to " +  user[0].name + " " + user[0].lastName );
+    setTimeout(() => {
+      window.location.reload();
+    }, 2000);
+   
     console.log("foundStudent:", subjectFoundStudent); // Verifica si foundStudent tiene el valor esperado
 
   };
 
+  const [subjectValue, setSubjectValue] = useState('');
+
+  
 
   return (
     <>
@@ -93,15 +101,37 @@ const AddSubject = () => {
                   <h1>Welcome {student.name} {student.lastName}</h1>
                   <form class="max-w-sm mx-auto">
                     <label for="subjects" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-                    <select id="subjects" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select
+
+                      id="subjects"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      value={subjectValue}
+                      onChange={e => setSubjectValue(e.target.value)}
+                    >
+
                       <option selected>Choose a Subject</option>
                       <option
-                        value="US"
+                        value="computer science"
+                      >
+                        Computer
+                      </option>
+                      <option
+                        value="math"
+                      >
+                        Math
+                      </option>
+                      <option
+                        value="english"
+                      >
+                        English
+                      </option>
+                      <option
+                        value="biology"
+                      >
+                        Biology
+                      </option>
 
-                      >United States</option>
-                      <option value="CA">Canada</option>
-                      <option value="FR">France</option>
-                      <option value="DE">Germany</option>
+
                     </select>
                   </form>
                   <button onClick={handleAddSubject}>Add Subject</button>
